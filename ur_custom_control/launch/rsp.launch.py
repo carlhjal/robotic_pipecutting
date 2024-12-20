ae464c381bdc72cd057b459695e3660dd5d3ba1d
Carl <lovehult98@gmail.com>
2024-12-20 18:13:47 +0200

    working moveit configuration (manually edit moveit_controllers.yaml)

--- ur_custom_control/launch/rsp.launch.py
+++ ur_custom_control/launch/rsp.launch.py
@@ -74,7 +74,7 @@ def generate_launch_description():
                 "ur20",
                 "ur30",
             ],
-            default_value="ur20",
+            default_value="ur5",
         )
     )
     declared_arguments.append(
