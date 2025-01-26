2ddf425c7c85e98c0f7c3118dde5da7bb7865af3
Carl Hult <lovehult98@gmail.com>
2025-01-26 21:32:45 +0200

    collision object spawner

---
+++ moveit_cart_path/include/collision_object_spawner.hpp
@@ -0,0 +1,22 @@
+#include <shape_msgs/msg/solid_primitive.hpp>
+#include <moveit_msgs/msg/collision_object.hpp>
+#include <rosidl_runtime_cpp/bounded_vector.hpp>
+#include <shape_msgs/msg/solid_primitive.hpp>
+#include <moveit/planning_scene_interface/planning_scene_interface.hpp>
+#include <string>
+
+class CollisionObjectSpawner{
+public:
+    explicit CollisionObjectSpawner(const std::string &frame_id);
+
+    int spawnObj(
+        const std::string &id,
+        const shape_msgs::msg::SolidPrimitive::_type_type &primitive_type,
+        const rosidl_runtime_cpp::BoundedVector<double, 3> &dimensions,
+        const geometry_msgs::msg::Pose &pose
+    );
+    
+private:
+    std::string frame_id_;
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
+};
--- moveit_cart_path/src/moveit_cart_path_test.cpp
+++ moveit_cart_path/src/moveit_cart_path_test.cpp
@@ -9,60 +9,60 @@
 
 int main(int argc, char * argv[])
 {
-  rclcpp::init(argc, argv);
-  auto const node = std::make_shared<rclcpp::Node>("cartesian_path");
-  const double jump_threshold = 0.0;
-  const double eef_step = 0.02;
-  moveit_msgs::msg::RobotTrajectory trajectory;
+    rclcpp::init(argc, argv);
+    auto const node = std::make_shared<rclcpp::Node>("cartesian_path");
+    const double jump_threshold = 0.0;
+    const double eef_step = 0.02;
+    moveit_msgs::msg::RobotTrajectory trajectory;
 
-  auto const logger = rclcpp::get_logger("cartesian_path");
+    auto const logger = rclcpp::get_logger("cartesian_path");
 
-  // SingleThreadedExecutor to get current pose of the robot
-  rclcpp::executors::SingleThreadedExecutor executor;
-  executor.add_node(node);
-  auto spinner = std::thread([&executor]() { executor.spin(); });
+    // SingleThreadedExecutor to get current pose of the robot
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(node);
+    auto spinner = std::thread([&executor]() { executor.spin(); });
 
-  using moveit::planning_interface::MoveGroupInterface;
-  auto move_group_interface = MoveGroupInterface(node, "ur_arm");
+    using moveit::planning_interface::MoveGroupInterface;
+    auto move_group_interface = MoveGroupInterface(node, "ur_arm");
 
-  //x - forward(+) and backward(-)
-  //y - left(+) and right(-)
-  //z - up(+) and down(-)
+    //x - forward(+) and backward(-)
+    //y - left(+) and right(-)
+    //z - up(+) and down(-)
 
-  std::vector<geometry_msgs::msg::Pose> waypoints;
-  
-  geometry_msgs::msg::Pose start_pose = move_group_interface.getCurrentPose().pose;
+    std::vector<geometry_msgs::msg::Pose> waypoints;
 
-  start_pose.position.z -= 0.2;
+    geometry_msgs::msg::Pose start_pose = move_group_interface.getCurrentPose().pose;
 
-  waypoints.push_back(start_pose);
+    start_pose.position.z -= 0.2;
 
-  double fraction = move_group_interface.computeCartesianPath(waypoints, eef_step,   jump_threshold, trajectory);
-  RCLCPP_INFO(logger, "Visualizing Cartesian path plan (%.2f%% achieved)", fraction * 100.0);
+    waypoints.push_back(start_pose);
 
-  double radius = 0.15;
-  int num_points = 20;
-  double center_y = start_pose.position.y;
-  double center_z = start_pose.position.z;
+    double fraction = move_group_interface.computeCartesianPath(waypoints, eef_step,   jump_threshold, trajectory);
+    RCLCPP_INFO(logger, "Visualizing Cartesian path plan (%.2f%% achieved)", fraction * 100.0);
 
-  for (int i = 0; i <= num_points; ++i)
-  {
-    double theta = 2.0 * M_PI * i / num_points; 
-    geometry_msgs::msg::Pose waypoint = start_pose;
-    waypoint.position.x = center_y + radius * cos(theta);
-    waypoint.position.z = center_z + radius * sin(theta);
-    waypoints.push_back(waypoint);
-  }
+    double radius = 0.15;
+    int num_points = 20;
+    double center_y = start_pose.position.y;
+    double center_z = start_pose.position.z;
 
-  fraction = move_group_interface.computeCartesianPath(waypoints, eef_step,   jump_threshold, trajectory);
-  RCLCPP_INFO(logger, "Visualizing Cartesian path plan (%.2f%% achieved)", fraction * 100.0);
+    for (int i = 0; i <= num_points; ++i)
+    {
+      double theta = 2.0 * M_PI * i / num_points; 
+      geometry_msgs::msg::Pose waypoint = start_pose;
+      waypoint.position.x = center_y + radius * cos(theta);
+      waypoint.position.z = center_z + radius * sin(theta);
+      waypoints.push_back(waypoint);
+    }
 
-  // Check if complete path is possible and execute the trajectory
-  if(fraction == 1){
-    move_group_interface.execute(trajectory);
-  }
-  
-  rclcpp::shutdown();
-  spinner.join();
-  return 0;
+    fraction = move_group_interface.computeCartesianPath(waypoints, eef_step,   jump_threshold, trajectory);
+    RCLCPP_INFO(logger, "Visualizing Cartesian path plan (%.2f%% achieved)", fraction * 100.0);
+
+    // Check if complete path is possible and execute the trajectory
+    if(fraction == 1){
+      move_group_interface.execute(trajectory);
+    }
+
+    rclcpp::shutdown();
+    spinner.join();
+    return 0;
 }
