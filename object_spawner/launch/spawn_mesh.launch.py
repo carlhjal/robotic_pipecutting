acf49637fca85c9ff66f0b72162a08e451e8d5eb
Carl Hult <lovehult98@gmail.com>
2025-02-26 00:40:39 +0200

    small fix

--- object_spawner/launch/spawn_mesh.launch.py
+++ object_spawner/launch/spawn_mesh.launch.py
@@ -4,13 +4,13 @@ import launch_ros.actions
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    package_name = "reach_config"  # Replace with your actual package name
+    package_name = "reach_config"
     mesh_file = "config/cylinder_lower_away.ply"
     mesh_path = os.path.join(get_package_share_directory(package_name), mesh_file)
     print(mesh_path)
     return launch.LaunchDescription([
         launch_ros.actions.Node(
-            package="object_spawner",  # Replace with your actual package name
+            package="object_spawner",
             executable="mesh_spawner",
             name="mesh_spawner",
             output="screen",
