9830d751d45af81982ac82feb014da1b84f9a7c2
Carl Hult <lovehult98@gmail.com>
2025-05-09 23:03:43 +0300

    comments

--- trajectory_seed_generator/include/seed_gen.hpp
+++ trajectory_seed_generator/include/seed_gen.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file seed_gen.hpp
+ * @author Carl Hjalmar Love Hult
+ * @brief Attempts to find a viable seed joint state for a cartesian trajectory
+ * @version 0.1
+ * @date 2025-05-09
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
 #include "moveit_msgs/msg/robot_trajectory.hpp"
 #include <moveit/kinematics_base/kinematics_base.hpp>
 #include <moveit/move_group_interface/move_group_interface.hpp>
@@ -17,6 +28,21 @@
 #include <moveit/move_group_interface/move_group_interface.hpp>
 #include <moveit_msgs/msg/robot_trajectory.hpp>
 
+
+/**
+ * @brief Returns a vector of joint states for a seed state that will allow execution of a cartesian trajectory
+ * 
+ * @param current_pose 
+ * @param waypoints 
+ * @param scene 
+ * @param model 
+ * @param move_group 
+ * @param group_name 
+ * @param eef_step 
+ * @param num_trials 
+ * @param ik_timeout 
+ * @return std::vector<double> 
+ */
 std::vector<double> get_viable_seed_state(
     const geometry_msgs::msg::Pose &current_pose,
     const std::vector<geometry_msgs::msg::Pose> &waypoints,
